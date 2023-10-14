b98e6bbdb33f50b48a0280e9c49b49c74de23a44
Blizzark <blizzark@ya.ru>
2023-10-14 15:53:29 +0300

    Прокомментировал работу

--- PatternCommandEducation/Program.cs
+++ PatternCommandEducation/Program.cs
@@ -6,14 +6,14 @@ public class Program
     {
         Console.WriteLine("Работа на кухне");
 
-        Washer washer = new Washer(); 
-        WasherCommand washerCommand = new(washer);
-        ChefInvoker chef = new ChefInvoker();
-        chef.SetCommand(washerCommand);
-        chef.SendСommand();
+        Washer washer = new Washer(); // создал мойщика. Может содержать кучу методов
+        WasherCommand washerCommand = new(washer); // прослойка в виде того, что именно мойщик делает (собирает методы в 2 команды (старта и отмена)). Реализация интерфейса
+        ChefInvoker chef = new ChefInvoker(); // создал Вызывателя команд
+        chef.SetCommand(washerCommand); // установил команды
+        chef.SendСommand(); // дал команду к исполнению
 
 
         Console.WriteLine("\nКонец рабочего дня\n");
-        chef.CanselСommand();
+        chef.CanselСommand(); // дал команду отмены исполнения
     }
 }
